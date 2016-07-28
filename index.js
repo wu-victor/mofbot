19e1f680c622ad5c4c76ff1ea76f083b3e837106
Victor Wu <vwu@clm-vwu.enova.com>
2016-07-28 15:55:00 -0500

    next payment

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
+var express = require("express");
+var bodyParser = require("body-parser");
+var request = require("request");
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 3000));
 
-// Server frontpage
-app.get('/', function (req, res) {
-  res.send('Mobile Only Financial - Facebook Messenger Bot');
+app.get("/", function (req, res) {
+  res.send("Mobile Only Financial - Facebook Messenger Bot");
 });
 
-// Facebook Webhook
-app.get('/webhook', function (req, res) {
-  if (req.query['hub.verify_token'] === 'mofbot_verify_token') {
-    res.send(req.query['hub.challenge']);
+app.get("/webhook", function (req, res) {
+  if (req.query["hub.verify_token"] === "mofbot_verify_token") {
+    res.send(req.query["hub.challenge"]);
   } else {
-    res.send('Invalid verify token');
+    res.send("Invalid verify token");
   }
 });
 
-// handler receiving messages
-app.post('/webhook', function (request, response) {
+app.post("/webhook", function (request, response) {
   var events = request.body.entry[0].messaging;
-  console.log('********* events **********');
+  console.log("********* events **********");
   console.log(events);
   for (i = 0; i < events.length; i++) {
     var event = events[i];
       if (event.message && event.message.text) {
 
         var responseType = getResponseType(event.message.text);
-        if (responseType === 'RATES') {
-          sendImage(event.sender.id, 'https://dl.dropboxusercontent.com/u/6677157/rates.png');
-        } else if (responseType === 'PAYMENT_PLAN') {
-          sendMessage(event.sender.id, {text: 'You have options'});
-        } else if (responseType === 'NEXT_PAYMENT') {
-          sendMessage(event.sender.id, {text: 'Your next payment'});
+        if (responseType === "RATES") {
+          sendRates(event.sender.id);
+        } else if (responseType === "PAYMENT_PLAN") {
+          sendMessage(event.sender.id, {text: "You have options"});
+        } else if (responseType === "NEXT_PAYMENT") {
+          sendNextPayment(event.sender.id, {text: "Your next payment"});
         } else {
-          sendMessage(event.sender.id, {text: 'Sentiment response'});
+          sendMessage(event.sender.id, {text: "Sentiment response"});
         }
       }
   }
@@ -46,44 +43,67 @@ app.post('/webhook', function (request, response) {
 });
 
 function getResponseType(message) {
-  if (message.toLowerCase().includes('rate') || message.toLowerCase().includes('term')) {
-    return 'RATES';
-  } else if (message.toLowerCase().includes('can\'t pay') || message.toLowerCase().includes('cannot pay')) {
-    return 'PAYMENT_PLAN';
-  } else if (message.toLowerCase().includes('next payment')) {
-    return 'NEXT_PAYMENT';
+  if (message.toLowerCase().includes("rate") || message.toLowerCase().includes("term")) {
+    return "RATES";
+  } else if (message.toLowerCase().includes("can't pay") || message.toLowerCase().includes("cannot pay")) {
+    return "PAYMENT_PLAN";
+  } else if (message.toLowerCase().includes("next payment")) {
+    return "NEXT_PAYMENT";
   } else {
-    return 'SENTIMENT';
+    return "SENTIMENT";
   }
 };
 
-function sendImage(recipientId, imageUrl) {
+function sendRates(recipientId) {
   var message = {
-    'attachment': {
-      'type': 'image',
-      'payload': {
-        'url': imageUrl
+    "attachment": {
+      "type": "image",
+      "payload": {
+        "url": "https://dl.dropboxusercontent.com/u/6677157/rates.png"
       }
     }
-  }
+  };
   sendMessage(recipientId, message);
 }
 
-// generic function sending messages
+function sendNextPayment(recipientId) {
+  var message = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [
+          {
+            "title": "Amount",
+            "subtitle": "$53.72",
+          }, {
+            "title": "Date",
+            "subtitle": "August 15, 2016",
+          }, {
+            "title": "Payment Method",
+            "subtitle": "Visa debit card ending in 4215"
+          }
+        ]
+      }
+    }
+  };
+  sendMessage(recipientId, message);
+}
+
 function sendMessage(recipientId, message) {
   request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
+    url: "https://graph.facebook.com/v2.6/me/messages",
     qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-    method: 'POST',
+    method: "POST",
     json: {
       recipient: {id: recipientId},
       message: message,
     }
   }, function(error, response, body) {
     if (error) {
-      console.log('Error sending message: ', error);
+      console.log("Error sending message: ", error);
     } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
+      console.log("Error: ", response.body.error);
     }
   });
 };
