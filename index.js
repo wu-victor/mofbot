59d4c2488deed74ed5406c64c8402b4729cad5b2
Victor Wu <vwu@clm-vwu.enova.com>
2016-07-28 15:08:36 -0500

    rates

--- index.js
+++ index.js
@@ -25,15 +25,28 @@ app.get('/webhook', function (req, res) {
 app.post('/webhook', function (request, response) {
   var events = request.body.entry[0].messaging;
   console.log('********* events **********');
-  console.log(events);  
+  console.log(events);
   for (i = 0; i < events.length; i++) {
     var event = events[i];
       if (event.message && event.message.text) {
+
+        var responseType = responseType(event.message);
+        if (responseType === 'RATES') {
+          sendMessage(event.sender.id, {text: 'These are MOF\'s awesome rates!'});  
+        }
         sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
       }
   }
   response.sendStatus(200);
 });
+
+function responseType(message) {
+  if (message.toLowerCase().includes('rate')) {
+    return 'RATES';
+  } else {
+    return 'SENTIMENT';
+  }
+}
 
 // generic function sending messages
 function sendMessage(recipientId, message) {
