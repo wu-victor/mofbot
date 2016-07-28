08692968830ccdd0f7e58b7742ec9fe397ffac44
Victor Wu <vwu@clm-vwu.enova.com>
2016-07-28 13:23:41 -0500

    console log

--- index.js
+++ index.js
@@ -22,8 +22,10 @@ app.get('/webhook', function (req, res) {
 });
 
 // handler receiving messages
-app.post('/webhook', function (req, res) {
-  var events = req.body.entry[0].messaging;
+app.post('/webhook', function (request, response) {
+  console.log('********* request **********');
+  console.log(request);
+  var events = request.body.entry[0].messaging;
   for (i = 0; i < events.length; i++) {
     var event = events[i];
       if (event.message && event.message.text) {
